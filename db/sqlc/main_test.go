a3000aa8bbd1d3c3324edaf592578c8f5cd56c64
Mike Taylor <miketaylor@mikes-mbp.home>
2023-09-05 14:25:05 +0100

    initial commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package sqlc
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://postgres:123456@192.168.1.83:5432/simplebank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to the db:", err)
+	}
+
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
