51f1a3a1b8b6bba2f7f54611d3f352bba816fd29
Mike Taylor <mike_cat_taylor@hotmail.com>
2023-09-12 12:49:14 +0100

    a bit of swagger

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -44,12 +44,15 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create access token")
 	}
 
+	// get the metadata
+	mtdt := server.extractMetadata(ctx)
+
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
