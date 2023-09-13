8bbcab5df1c0aa9340cacf54b80867325a0e7692
Mike Taylor <mike_cat_taylor@hotmail.com>
2023-09-13 15:33:27 +0100

    make redis use the same log format

--- worker/processor.go
+++ worker/processor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 
 	db "github.com/mikcatta/simple_bank/db/sqlc"
 )
@@ -31,6 +32,14 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskPr
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().
+					Err(err).
+					Str("type", task.Type()).
+					Bytes("payload", task.Payload()).
+					Msg("process task failed")
+			}),
+			Logger: NewLogger(),
 		},
 	)
 
